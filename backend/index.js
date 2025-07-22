b3ac78e3325ed464b548932a4f31046bcb0fba94
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2025-07-22 22:59:14 +0200

    Allow the backend to talk to the Roon core

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
-import { server } from './src/server.js';
+import { roon, server } from './src/server.js';
 
 const PORT = 4001;
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on port ${PORT}.`);
 });
+
+roon.start_discovery();
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,44 @@
 import http from 'http';
+
 import express from 'express';
+import RoonApi from 'node-roon-api';
+import RoonApiBrowse from 'node-roon-api-browse';
+import RoonApiStatus from 'node-roon-api-status';
+import RoonApiTransport from 'node-roon-api-transport';
+
+let transport;
+let browseInstance;
 
 const app = express();
 const server = http.createServer(app);
 
-export { server };
+const roon = new RoonApi({
+  /* eslint-disable camelcase */
+  extension_id: 'com.roon-api-browser-backend.test',
+  display_name: 'Roon API Browser Extension',
+  display_version: '0.0.0',
+  publisher: 'Michael Pfeifer',
+  email: 'michael.k.pfeifer@googlemail.com',
+  website: 'https://github.com/michaelkpfeifer',
+
+  core_paired: async (core) => {
+    transport = core.services.RoonApiTransport;
+    browseInstance = new RoonApiBrowse(core);
+  },
+
+  core_unpaired: (/* core */) => {},
+  /* eslint-enable camelcase */
+});
+
+const serviceStatus = new RoonApiStatus(roon);
+
+roon.init_services({
+  /* eslint-disable camelcase */
+  required_services: [RoonApiTransport, RoonApiBrowse],
+  provided_services: [serviceStatus],
+  /* eslint-enable camelcase */
+});
+
+serviceStatus.set_status('All is good', false);
+
+export { roon, server };
